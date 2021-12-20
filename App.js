b4b165803f520ad2c994d9e7e0b131a8393315a6
caste77an <hancastellan@naver.com>
2021-12-20 16:31:02 +0900

    #4.7 More Interpolations

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Animated, Easing, TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, Easing, Pressable, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -7,36 +7,47 @@ const Container = styled.View`
   justify-content: center;
   align-items: center;
 `;
-
 const Box = styled.View`
   background-color: tomato;
   width: 200px;
   height: 200px;
 `;
-
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
   const [up, setUp] = useState(false);
-  const Y = useRef(new Animated.Value(0)).current;
+  const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current;
   const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
-    Animated.timing(Y, {
-      toValue: up ? 200 : -200,
-      useNativeDriver: true,
-      easing: Easing.circle,
+    Animated.timing(POSITION, {
+      toValue: up ? 300 : -300,
+      useNativeDriver: false,
+      duration: 1000,
     }).start(toggleUp);
   };
-
+  const rotation = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["-360deg", "360deg"],
+  });
+  const borderRadius = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 0],
+  });
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
+  });
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
+      <Pressable onPress={moveUp}>
         <AnimatedBox
           style={{
-            transform: [{ translateY: Y }],
+            borderRadius,
+            backgroundColor: bgColor,
+            transform: [{ rotateY: rotation }, { translateY: POSITION.y }],
           }}
         />
-      </TouchableOpacity>
+      </Pressable>
     </Container>
   );
 }
